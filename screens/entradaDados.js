4fc88b719ace2bccc8f4a08541e5046c96f497b0
JDubas <Rm76153@fiap.com.br>
2024-04-23 10:26:58 -0300

    Processamento de dados

--- screens/entradaDados.js
+++ screens/entradaDados.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import react from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from 'react';
+
 
 export default function entradaDados({ navigation }) {
-    const x = {
-        teste: 'teste'
-    }
+  const [valor1, setValor1] = useState('');
+  const [valor2, setValor2] = useState('');
+  const [valor3, setValor3] = useState('');
 
+  const calcular = () => {
+    const dados = {
+        valor1: valor1,
+        valor2: valor2,
+        valor3: valor3
+    };
+    navigation.navigate('saidaDados', { dados: dados });
+};
     return (
-      <View style={styles.container}>
-        <Text>Página para calculo da lei calor latente</Text>
-        <Button
-          title="Calcular"
-          onPress={() => navigation.navigate('saidaDados')}/>
-        <StatusBar style="auto" />
-      </View>
+       <View style={styles.container}>
+            <Text>Página para cálculo da lei calor latente</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Digite valor 1"
+                onChangeText={text => setValor1(text)}
+                value={valor1}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Digite valor 2"
+                onChangeText={text => setValor2(text)}
+                value={valor2}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Digite valor 3"
+                onChangeText={text => setValor3(text)}
+                value={valor3}
+            />
+            <Button
+                title="Calcular"
+                onPress={calcular}
+            />
+            <StatusBar style="auto" />
+        </View>
     );
   }
 
--- screens/saidaDados.js
+++ screens/saidaDados.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import react from 'react';
 
 
 
-export default function saidaDados({ navigation }) {
 
+export default function saidaDados({ navigation, route }) {
+  const dadosRecebidos = route.params.dados;
+
+
+
+  
     return (
       <View style={styles.container}>
-        <Text> {x.teste} </Text> 
+            <Text>Valor 1: {dadosRecebidos.valor1}</Text>
+            <Text>Valor 2: {dadosRecebidos.valor2}</Text>
+            <Text>Valor 3: {dadosRecebidos.valor3}</Text>
         <Button
           title="Sair"
           onPress={() => navigation.goBack()}/>
