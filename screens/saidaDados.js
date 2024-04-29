52fb3f79329ea8bbd3db43ef2e5841d6d931bbc1
pedrohenriquelourenco <rm98402@fiap.com.br>
2024-04-29 14:02:20 -0300

    alteração img

--- screens/saidaDados.js
+++ screens/saidaDados.js
@@ -1,42 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
-
-
-
-export default function saidaDados({ navigation, route }) {
+export default function SaidaDados({ navigation, route }) {
   const dadosRecebidos = route.params.dados;
-
-
   const { massa, calorLatente } = dadosRecebidos;
 
-
   const calcularCalorLatente = () => {
-      const calorLatenteTotal = massa * calorLatente;
-      return calorLatenteTotal;
+    const calorLatenteTotal = massa * calorLatente;
+    return calorLatenteTotal;
   };
 
-
-
-    return (
-      <View style={styles.container}>
-      <Text>Massa: {massa} kg</Text>
-      <Text>Calor latente específico: {calorLatente} J/kg</Text>
-      <Text>Calor latente total: {calcularCalorLatente()} J</Text>
-      <Button
-          title="Sair"
-          onPress={() => navigation.goBack()}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Resultado do Cálculo</Text>
+      
+      <Image
+        source={require('../assets/calor.jpeg')}
+        style={styles.image}
+        resizeMode="contain"
       />
-      <StatusBar style="auto" />
-  </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+
+      <Text style={styles.text}>Massa: {massa} kg</Text>
+      <Text style={styles.text}>Calor latente específico: {calorLatente} J/kg</Text>
+      <Text style={styles.text}>Calor latente total: {calcularCalorLatente()} J</Text>
+      
+      <Button
+        title="Sair"
+        onPress={() => navigation.goBack()}
+        style={styles.button}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  image: {
+    width: 200, // Ajuste de acordo com a largura desejada da imagem
+    height: 200, // Ajuste de acordo com a altura desejada da imagem
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'left',
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: '#007bff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+});
